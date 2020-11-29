40e01b7e0a9a63cfd3cad0eb6ebbb0976a6ca6f9
Dinesh101041 <dinesh101041@gmail.com>
2020-11-29 20:45:29 +0530

    displaying pictures

--- src/comps/Imagegrid.js
+++ src/comps/Imagegrid.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Usefirestore from '../firebase/Usefirestore';
 
 const Imagegrid=()=>{
+    const{docs}=Usefirestore('images')
+    console.log(docs);
     return(
-        <div className='Img-grid'>
-            images
+        <div className='img-grid'>
+            {docs && docs.map(doc =>(
+                <div className='img-wrap' key={doc.id}>
+                    <img src={doc.url} alt='upload pic'></img>
+                </div>
+            ))}
         </div>
     )
 }
--- src/firebase/Usefirestore.js
+++ src/firebase/Usefirestore.js
@@ -1,8 +1,27 @@
-import {useState,useEfffect} from 'react';
+import {useState,useEffect} from 'react';
 import {projectfirestore} from '../firebase/config';
 
-const usefirestore = (collection)=>{
+// collecttion is images from firebase
+const Usefirestore = (collection)=>{
     const[docs,setDocs] = useState([]);
 
-    useEfffect
+    useEffect(()=>{
+       const unsub = projectfirestore.collection(collection)
+        // oreder the image in descending order
+        .orderBy('createdAt','desc')
+        .onSnapshot(snap=>{
+            // get the images in array 
+            let document=[];
+            snap.forEach(doc =>{
+                document.push({...doc.data(),id: doc.id});
+            });
+            setDocs(document);
+
+        });
+        return()=>unsub();
+    },[collection])
+
+    return{docs};
 }
+
+export default Usefirestore;
